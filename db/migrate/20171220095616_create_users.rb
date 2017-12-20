954a964c17ab03034b3c5643b21010dd753f0520
Алексей Мехоношин <ruskidecko@gmail.com>
2017-12-20 13:35:57 +0300

    Lesson 3. Создание моделей

--- db/migrate/20171220095616_create_users.rb
+++ db/migrate/20171220095616_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :email
       t.string :first_name
       t.string :last_name
-      t.digest :password
+      t.string :password_digest
 
       t.timestamps
     end
