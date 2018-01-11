4a6ee9e9a11c5efbaf86f5b9205446c18282a4e8
rusdec <ruskidecko@gmail.com>
2018-01-11 21:25:40 +0300

    Lesson 6. Переимен. scope :corrects -> :correct

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  scope :corrects, -> { where(correct: true) }
+  scope :correct, -> { where(correct: true) }
   
   validates :body, presence: true
   validates :question_id, presence: true,
