831739cec83ed7a4555e9eb0502fd448322a90fd
rusdec <ruskidecko@gmail.com>
2018-01-15 21:24:14 +0300

    Lesson 7. Мелкие правки

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,10 +1,17 @@
 class Setting < ApplicationRecord
   
-  scope :value_by_setting, ->(setting) { where(setting: setting).pluck(:value).first }
-  scope :max_answers, -> { value_by_setting(:max_answers).to_i }
-  scope :min_answers, -> { value_by_setting(:min_answers).to_i }
-  scope :answers_range, -> { (min_answers..max_answers) }
-
   validates :name, presence: true
   validates :value, presence: true
+
+  class << self
+    def answers_range
+      (min_answers..max_answers)
+    end
+    def max_answers
+      where(setting: :max_answers).pluck(:value).first.to_i 
+    end
+    def min_answers
+      where(setting: :min_answers).pluck(:value).first.to_i  
+    end
+  end
 end
