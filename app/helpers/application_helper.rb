98360d5f840d30048e072fb33d34b32ef84d2290
rusdec <ruskidecko@gmail.com>
2018-01-18 22:26:56 +0300

    Lesson 8.
    - Добавлены хелперы
    - Добавлено отображение кол-во вопросов в списке тестов

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  
+  def current_year
+    Time.now.strftime('%Y')
+  end
+
+  def github_url(params)
+    url = "//github.com/#{params[:author]}/#{params[:repo]}"
+
+    link_to params[:title], url, target: '_blank'
+  end
+
+  def thinknetica_url(title)
+    link_to title, '//thinknetica.com/', target: '_blank'
+  end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,13 @@
 module QuestionsHelper
+
+  def question_header
+    if @question.respond_to?(:persisted?) && @question.persisted?
+      action = 'Редактирование'
+    else
+      action = 'Создание'
+    end
+
+    tag.h1 "#{action} вопроса теста \"#{@test.title}\""
+  end
+
 end
