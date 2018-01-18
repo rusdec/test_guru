7cc2f1fa94bd95b606c0c6de9d4810742457176d
rusdec <ruskidecko@gmail.com>
2018-01-18 23:08:36 +0300

    Lesson 8. Правка хелпера question_header

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,11 +1,7 @@
 module QuestionsHelper
 
   def question_header
-    if @question.respond_to?(:persisted?) && @question.persisted?
-      action = 'Редактирование'
-    else
-      action = 'Создание'
-    end
+    action = @question.persisted? ? 'Редактирование' : 'Создание'
 
     tag.h1 "#{action} вопроса теста \"#{@test.title}\""
   end
