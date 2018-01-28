bfb1c1caf220189247f390066f6b8bc526848791
rusdec <ruskidecko@gmail.com>
2018-01-28 16:59:38 +0300

    Lesson 10. Задание 2 - Создайте хэлпер для flash сообщений

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,12 @@ module ApplicationHelper
   def thinknetica_url(title)
     link_to title, '//thinknetica.com/', target: '_blank'
   end
+
+  def flash_alert
+    flash_message(:alert)
+  end
+
+  def flash_message(param)
+    content_tag :p, flash[param], class: 'flash alert' if flash[param]
+  end
 end
