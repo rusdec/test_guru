ea77ae1eb5468a9f3cd6b77880a9174e9f234d50
rusdec <ruskidecko@gmail.com>
2018-01-28 21:21:47 +0300

    Lesson 10. Задание 7. Добавил ubiq-индекс в таблицу

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124041352) do
+ActiveRecord::Schema.define(version: 20180128171855) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20180124041352) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
