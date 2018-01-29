88f2b079db53a75a41f8c918bfc47f846c80d2f5
rusdec <ruskidecko@gmail.com>
2018-01-29 13:01:16 +0300

    рефакторинг find_by(id: ..) -> find()

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
