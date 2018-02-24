085f2f4f877180700079ed212803c52bb2a6017f
rusdec <ruskidecko@gmail.com>
2018-02-24 22:23:57 +0300

    Lesson 15. Задание 1 - Рефакторинг сортировки

---
+++ app/assets/javascripts/sort_elements_collection.js
@@ -0,0 +1,49 @@
+class SortElementsCollection {
+  constructor(nodes) {
+    this._elements = this.toSortableElements(nodes)
+  }
+
+  // return array of Nodes
+  nodes(elements) {
+    elements = (elements === undefined) ? this._elements : elements
+    let nodes = [];
+    for(let i = 0; i < elements.length; i++) {
+      nodes.push(elements[i].node)
+    }
+
+    return nodes;
+  }
+
+  // return array of SortableElement objects
+  toSortableElements(nodes) {
+    let elements = []
+    for(let i = 0; i < nodes.length; i++) {
+      elements.push(this.toSortableElement(nodes[i]))
+    }
+    return elements;
+  }
+
+  toSortableElement(node) {
+    return new SortableElement(node)
+  }
+
+  sortByDesc(param) {
+    var p = param
+    return this.nodes(this._elements.sort(function(x, y) {
+      x = Number.isNaN(Number.parseFloat(x[p])) ? x[p] : Number.parseFloat(x[p])
+      y = Number.isNaN(Number.parseFloat(y[p])) ? y[p] : Number.parseFloat(y[p])
+
+      return (x < y) ? 1 : (x === y) ? 0 : -1;
+    }));
+  }
+
+  sortByAsc(param) {
+    var p = param
+    return this.nodes(this._elements.sort(function(x, y) {
+      x = Number.isNaN(Number.parseFloat(x[p])) ? x[p] : Number.parseFloat(x[p])
+      y = Number.isNaN(Number.parseFloat(y[p])) ? y[p] : Number.parseFloat(y[p])
+
+      return (x < y) ? -1 : (x === y) ? 0 : 1;
+    }));
+  }
+}
