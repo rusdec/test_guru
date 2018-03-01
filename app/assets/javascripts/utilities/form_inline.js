740da0e6af4f2e6cca3c96bd786b46836c1d4624
rusdec <ruskidecko@gmail.com>
2018-03-01 21:57:26 +0300

    Lesson 16. Быстрое редактирование

---
+++ app/assets/javascripts/utilities/form_inline.js
@@ -0,0 +1,39 @@
+document.addEventListener('turbolinks:load', function() {
+  var elements = document.querySelectorAll('.form-inline-link')
+
+  if (elements.length) {
+    for (i = 0; i < elements.length; i++) {
+      elements[i].addEventListener('click', formInlineLinkHandler)
+    }
+  }
+
+  var errors = document.querySelector('.resource-errors')
+  if (errors) {
+    formLinkHandler(errors.dataset.resourceId)
+  }
+
+
+  function formInlineLinkHandler(event) {
+    event.preventDefault();
+    var testId = this.dataset.testId
+    formLinkHandler(testId)
+  }
+
+  function formLinkHandler(id) {
+    var dataSelector = `[data-test-id="${id}"]`
+    var form = document.querySelector(`.form-inline${dataSelector}`)
+    var title = document.querySelector(`.test-title${dataSelector}`)
+    var link = document.querySelector(`.form-inline-link${dataSelector}`)
+    
+    if (form.classList.contains('hidden')) {
+      title.classList.add('hidden');
+      form.classList.remove('hidden');
+      link.textContent = "Отменить";
+    } else {
+      title.classList.remove('hidden');
+      form.classList.add('hidden');
+      link.textContent = "Редакт. быстр."
+    }
+  }
+
+});
