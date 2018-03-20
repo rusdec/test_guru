1ce8eb312d64ba158ed9bd764b3ec5dedc5de3bd
rusdec <ruskidecko@gmail.com>
2018-03-20 15:15:44 +0300

    убран puts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    puts current_user.test_passages.inspect
     @user_badges = current_user.badges.uniq
     @test_passages = current_user.test_passages.completed
     @badges = Badge.active
